59d1dc87f77f46b05419cf624ae01a7bb85bae1a
GitHub Action <action@github.com>
2022-05-16 13:52:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/kawaiifoxx/inlinecalculator/MyBundle.kt
+++ src/main/kotlin/com/github/kawaiifoxx/inlinecalculator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.kawaiifoxx.inlinecalculator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/kawaiifoxx/inlinecalculator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kawaiifoxx.inlinecalculator.services
+
+import com.github.kawaiifoxx.inlinecalculator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
